917ab799e06f4443148b9aa40a43a1dd698c7418
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-07-11 23:37:51 +0530

    car image function compleated

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,23 @@
 "use client"
 
 import { use, useState } from "react"
+import Image from "next/image"
 
 import SearchManufacturer from "./SearchManufacturer"
+
+const SearchButton = ({ otherClasses }:{otherClasses:string}) => (
+
+  <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="Search"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+
+)
 
 const SearchBar = () => {
 
@@ -18,6 +33,11 @@ const SearchBar = () => {
                 setManufacturer={setManufacturer}
             />
 
+          <SearchButton otherClasses="sm:hidden"/>
+
+        </div>
+        <div>
+          
         </div>
     </form>
   )
