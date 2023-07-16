de18615628a6b18c70ebf6cdc1a3bbfda8a39ffb
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-07-17 01:54:14 +0530

    search compleated

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import React from "react"
+import { useRouter } from "next/navigation"
 import { use, useState } from "react"
 import Image from "next/image"
 
@@ -23,8 +25,39 @@ const SearchBar = () => {
 
   const [manufacturer, setManufacturer] = useState('')
   const [model, setModel] = useState('');
+  const router = useRouter();
 
-  const handleSearch = () => {}
+  const handleSearch = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (manufacturer === '' && model === '') {
+      alert('Please fill in the search bar');
+      return;
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
+  }
+
+  //code for update search params
+  const updateSearchParams = (model:string, manufacturer:string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    
+    if(model) {
+      searchParams.set('model', model);
+    }else{
+      searchParams.delete('model');
+    }
+
+    if(manufacturer) {
+      searchParams.set('manufacturer', manufacturer);
+    }else{
+      searchParams.delete('manufacturer');
+    }
+
+    const newPathname =`${window.location.pathname}?${searchParams.toString()}`
+
+    router.push(newPathname);
+  }
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
